1e99850b704a5089f0b22bf0dca25c16f3a6ab97
nadim <nadim.alaa@hotmail.com>
2024-03-25 21:19:54 +0400

    added removing shape when creating order - links layout

--- app/components/PageLink.js
+++
@@ -1,9 +0,0 @@
-function PageLink({ pageTitle }) {
-  return (
-    <div>
-      <h4>{pageTitle}</h4>
-    </div>
-  );
-}
-
-export default PageLink;
--- app/page.js
+++ app/page.js
@@ -1,17 +1,7 @@
-import Link from "next/link.js";
-import PageLink from "./components/PageLink.js";
+
 const HomePage = () => {
-  const pages = ["Press", "Chemicals", "Alloy"];
   return (
-    <div className="relative w-full">
-      {pages.map((page, index) => {
-        return (
-          <Link key={index} href={`/${page.toLocaleLowerCase()}`}>
-            <PageLink pageTitle={page} />
-          </Link>
-        );
-      })}
-    </div>
+    <h1>HomePage</h1>
   );
 };
 
