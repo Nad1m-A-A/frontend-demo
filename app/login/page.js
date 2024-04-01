4fc96f5f021cf4b961962ec2c8b0b93e133b007c
nadim <nadim.alaa@hotmail.com>
2024-04-01 16:01:29 +0400

    re-created login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,72 +1,45 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import capture_form_values from "../utils/capture_form_values";
+import httpRequest from "../actions/httpRequest";
+import { useState } from "react";
 const Login = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   if (localStorage.getItem("k6")) router.back();
 
-  const handleLogin = async () => {
-    setIsLoading(true);
-    setError(null);
+  const handleLogin = async (formValues) => {
+    setLoading(true);
+    const password = capture_form_values(formValues);
+    const [feedback] = await httpRequest(
+      ["http://localhost:5000/pass-code/validate"],
+      "POST",
+      [],
+      [password]
+    );
 
-    try {
-      if (!password) throw new Error("please provide password!");
-      const res = await fetch("/login/api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userInput: password,
-        }),
-      });
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-      const data = await res.json();
-      if (!data.isValid) throw new Error("Incorrect password!");
-      localStorage.setItem("k6", JSON.stringify(data));
-      router.replace("/");
-    } catch (error) {
-      setError(error.message);
-      console.error("Login error:", error);
-    } finally {
-      setIsLoading(false);
+    setLoading(false);
+    if (!feedback.isValid) {
+      setError("Wrong Password!");
+      return;
     }
+    localStorage.setItem("k6", JSON.stringify(feedback));
+    router.replace("/");
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleLogin();
-          }}
-        >
-          <div className="mb-4">
-            <input
-              placeholder="password"
-              type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded focus:outline-none focus:bg-blue-600"
-          >
-            {isLoading ? "Logging in..." : "Login"}
-          </button>
-          {error && <p className="text-red-500 mt-2">{error}</p>}
-        </form>
-      </div>
+    <div className="page flex justify-center h-screen">
+      <form className="flex flex-col gap-2" action={handleLogin}>
+        <input name="password" placeholder="password" required type="text" />
+        <button type="submit" className="w-full">
+          Login
+        </button>
+      </form>
+      <div className="text-red-600 text-center p-2">{error}</div>
+      {loading && (
+        <div className="text-blue-600 text-center p-2">Loading...</div>
+      )}
     </div>
   );
 };
