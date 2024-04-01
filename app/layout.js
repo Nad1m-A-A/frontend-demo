40f4ceb40966ea028f0c2d913dbc3a746a49bb47
nadim <nadim.alaa@hotmail.com>
2024-04-01 19:49:00 +0400

    error fix 3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-"use client";
+// "use client";
+// import { useEffect } from "react";
+// import { useRouter, usePathname } from "next/navigation";
 import "./globals.css";
-import { useRouter, usePathname } from "next/navigation";
-import { useEffect } from "react";
 import Link from "next/link.js";
 import Image from "next/image";
 import { Manrope } from "next/font/google";
@@ -12,45 +12,41 @@ const manrope = Manrope({
 
 export default function RootLayout({ children }) {
   const pages = ["Press", "Chemicals", "Alloy"];
-  const router = useRouter();
-  const pathname = usePathname();
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      if (!localStorage.getItem("k6")) {
-        router.replace("/login");
-      }
-    }
-  }, [router]);
-  const isLogin = pathname === "/login";
+  // const router = useRouter();
+  // const pathname = usePathname();
+  // useEffect(() => {
+  //   if (typeof window !== undefined) {
+  //     if (!localStorage.getItem("k6")) {
+  //       router.replace("/login");
+  //     }
+  //   }
+  // }, [router]);
+  // const isLogin = pathname === "/login";
 
   return (
     <html lang="en">
       <body className={manrope.className}>
-        {!isLogin && (
-          <>
-            <Image
-              priority
-              width={2200}
-              height={1800}
-              src="/3.jpg"
-              alt="Jewelry Width Image"
-              className="w-full h-96 object-cover"
-            />
-            <div className="nav">
-              {pages.map((page, index) => {
-                return (
-                  <Link
-                    className="link"
-                    key={index}
-                    href={`/${page.toLocaleLowerCase()}`}
-                  >
-                    {page}
-                  </Link>
-                );
-              })}
-            </div>
-          </>
-        )}
+        <Image
+          priority
+          width={2200}
+          height={1800}
+          src="/3.jpg"
+          alt="Jewelry Width Image"
+          className="w-full h-96 object-cover"
+        />
+        <div className="nav">
+          {pages.map((page, index) => {
+            return (
+              <Link
+                className="link"
+                key={index}
+                href={`/${page.toLocaleLowerCase()}`}
+              >
+                {page}
+              </Link>
+            );
+          })}
+        </div>
 
         {children}
       </body>
