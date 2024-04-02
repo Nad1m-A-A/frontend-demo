6d3a9ffdc0ec8cfe686309beaec869d3d5dcaa2e
nadim <nadim.alaa@hotmail.com>
2024-04-02 21:32:15 +0400

    removed allowedOrigins

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: [
-        "https://k6-navy.vercel.app",
-        "*.https://k6-navy.vercel.app",
-        "https://k6j.vercel.app",
-        "*.https://k6j.vercel.app",
-      ],
-    },
+
   },
 };
 
